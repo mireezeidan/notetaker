d85b85ea4118dff88ee8b54bb85e497ad377c6ce
Miree Zeidan <mireezeidan@gmail.com>
2023-01-12 10:28:47 -0600

    added code to save to database, as well as created routes for get and post

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
+
+// GET Route for retrieving all the tips
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`note added successfully 🚀`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+module.exports = notes;
